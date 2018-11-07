b4dcfc947e07a71a212c8acdff881bef60c71af0
Jaume Singla Valls <jaume.singla@privalia.com>
2018-11-07 10:19:47 +0100

    Removed FetchMiddleware

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export { default as fetchReducer } from './reducers/FetchReducer';
-export { fetchMiddleware } from './core/FetchMiddleware';
 export { default as fetchSaga} from './saga';
 export * from './reducers';
 
