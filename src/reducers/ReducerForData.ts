0c8038bc216ed0933af685f0e4fb98c74e47919d
Jaume Singla Valls <jaume.singlavalls@gmail.com>
2019-03-18 23:48:40 +0100

    Solving issues of the build

--- src/reducers/ReducerForData.ts
+++ src/reducers/ReducerForData.ts
@@ -1,9 +1,9 @@
 import { ReducerCallback, Action } from "../Types";
 
-export default (actionType: string, reducer:ReducerCallback) => (state =[], action:Action) => {
+export default (actionType: string, reducer:ReducerCallback<any>) => (state =[], action:Action) => {
     if (actionType === action.type) {
         let id = action.id!
-        let subState : any[] = state[id]
+        let subState : any = state[id]
         let newState = reducer(subState, action.payload)
         if (newState !== subState){
             let r : any[] = [...state]
